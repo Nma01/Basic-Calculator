d1fd63e7d5e79a4b6eee5dee51c2ad179eacd286
Nma01 <chidinmaoparahtina@yahoo.com>
2023-10-08 20:18:26 +0100

    Basic Calculator

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const display = document.querySelector('#operation');
+const buttons = document.querySelectorAll('button');
+
+
+
+buttons.forEach((btn) => {
+//   console.log(btn.id);
+  btn.onclick = () => {
+    if (btn.id == 'clear') {
+      display.innerText = '';
+    } else if (btn.id == 'back') {
+      let string = display.innerText.toString();
+      display.innerText = string.substr(0, string.length - 1);
+    } else if (display.innerText !== '' && btn.id == 'equals') {
+      display.innerText = eval(display.innerText);
+    } else if (display.innerText == '' && btn.id == 'equals') {
+      display.innerText = 'Empty!';
+      // setTimeout(()=>(display.innerText = ''), 3000);
+      setTimeout(clearEmpty, 3000);
+      function clearEmpty() {
+        display.innerText = '';
+      }
+    } else {
+      display.innerText += btn.id;
+    }
+  };
+});
